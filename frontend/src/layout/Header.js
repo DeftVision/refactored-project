0a92a9017e56ae1be930fb9a6965adc8d072b241
Gabriel <gjbarret@asu.edu>
2024-04-05 21:11:27 -0600

    added doc thumbnails

--- frontend/src/layout/Header.js
+++ frontend/src/layout/Header.js
@@ -23,17 +23,16 @@ export default function Header() {
                             <Nav.Link as={Link} to="/">Home</Nav.Link>
                             <Nav.Link as={Link} to="/dashboard">Dashboard</Nav.Link>
                             <Nav.Link as={Link} to="/announcements">Announcements</Nav.Link>
+                            <Nav.Link as={Link} to="/profile">Profile</Nav.Link>
                             <Nav.Link as={Link} to="/evaluations">Evaluations</Nav.Link>
                             <Nav.Link as={Link} to="/documents">Documents</Nav.Link>
-                            <Nav.Link as={Link} to="/profile">Profile</Nav.Link>
-
+                            <Nav.Link as={Link} to="/test">Testing</Nav.Link>
                             {user && user.role === 'Admin' && (
-                                <Nav.Link as={Link} to="/admin">Admin</Nav.Link>)
-                            }
+                                <Nav.Link as={Link} to="/admin">Admin</Nav.Link>)}
 
                             {user &&
                                 <Nav.Link onClick={logout}>Logout</Nav.Link>}
-                            <Nav.Link as={Link} to="/test">Testing</Nav.Link>
+
                         </Nav>
                     </Navbar.Collapse>
 
